d24af5c7bae08aaf7cf1752cc98519d741c253d9
Per Jahn <perjahn@gmail.com>
2018-04-03 15:54:45 +0000

    Added new backup tool for Grafana.

---
+++ BackupGrafana/Grafana.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace BackupGrafana
+{
+    class Grafana
+    {
+        public bool SaveDashboards(string serverurl, string username, string password, string folder)
+        {
+            BackupToGit.SecureLogger.SensitiveStrings.AddRange(new[] { username, password });
+
+            BackupToGit.Filesystem.RobustDelete(folder);
+
+            Log($"Creating directory: '{folder}'");
+            Directory.CreateDirectory(folder);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var creds = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", creds);
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                Log("Retrieving orgs.");
+                string url = $"{serverurl}/api/orgs";
+                string result;
+                try
+                {
+                    result = client.GetStringAsync(url).Result;
+                }
+                catch (AggregateException ex)
+                {
+                    Log($"Couldn't connect to Grafana server: '{url}' '{ex.Message}'");
+                    return false;
+                }
+
+                JArray orgs = JArray.Parse(result);
+
+                foreach (dynamic org in orgs)
+                {
+                    Log($"Switching to org: {org.id} '{org.name}'");
+                    url = $"{serverurl}/api/user/using/{org.id}";
+                    var postresult = client.PostAsync(url, null);
+                    result = postresult.Result.Content.ReadAsStringAsync().Result;
+
+                    Log("Searching for dashboards.");
+                    url = $"{serverurl}/api/search/";
+                    result = client.GetStringAsync(url).Result;
+
+                    JArray array = JArray.Parse(result);
+
+                    foreach (dynamic j in array)
+                    {
+                        Log($"Retrieving dashboard: '{j.uri}'");
+                        url = $"{serverurl}/api/dashboards/{j.uri}";
+                        result = client.GetStringAsync(url).Result;
+
+                        dynamic dashboard = JObject.Parse(result);
+
+                        string name = dashboard.meta.slug;
+
+                        dashboard.meta.Property("expires").Remove();
+                        dashboard.meta.Property("created").Remove();
+                        dashboard.meta.Property("updated").Remove();
+                        dashboard.meta.Property("updatedBy").Remove();
+                        dashboard.meta.Property("createdBy").Remove();
+                        dashboard.meta.Property("version").Remove();
+
+                        string filename = Path.Combine(folder, PrettyName($"{org.name}_{name}") + ".json");
+
+                        string pretty = dashboard.ToString(Newtonsoft.Json.Formatting.Indented);
+
+                        Log($"Saving: '{filename}'");
+                        File.WriteAllText(filename, pretty);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        string PrettyName(string name)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in name.ToCharArray())
+            {
+                if (char.IsLetterOrDigit(c) || c == '-' || c == '_')
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        static void Log(string message)
+        {
+            BackupToGit.SecureLogger.WriteLine(message);
+        }
+    }
+}
---
+++ BackupGrafana/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BackupGrafana
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            BackupToGit.Git git = new BackupToGit.Git();
+            BackupToGit.SecureLogger.Logfile = Path.Combine(Directory.GetCurrentDirectory(), "BackupGrafana.log");
+
+            if (args.Length != 3)
+            {
+                Log("Usage: BackupGrafana <serverurl> <username> <password>");
+                return 1;
+            }
+
+            string url = args[0];
+            string username = args[1];
+            string password = args[2];
+            string folder = "dashboards";
+
+            Grafana grafana = new Grafana();
+            if (!grafana.SaveDashboards(url, username, password, folder))
+            {
+                return 1;
+            }
+
+            string gitsourcefolder = folder;
+            string gitbinary = Environment.GetEnvironmentVariable("gitbinary");
+            string gitserver = Environment.GetEnvironmentVariable("gitserver");
+            string gitrepopath = Environment.GetEnvironmentVariable("gitrepopath");
+            string gitrepofolder = Environment.GetEnvironmentVariable("gitrepofolder");
+            string gitusername = Environment.GetEnvironmentVariable("gitusername");
+            string gitpassword = Environment.GetEnvironmentVariable("gitpassword");
+            string gitemail = Environment.GetEnvironmentVariable("gitemail");
+            bool gitsimulatepush = ParseBooleanEnvironmentVariable("gitsimulatepush", false);
+
+            if (string.IsNullOrEmpty(gitbinary) || string.IsNullOrEmpty(gitserver) || string.IsNullOrEmpty(gitrepopath) || string.IsNullOrEmpty(gitrepofolder) ||
+                string.IsNullOrEmpty(gitusername) || string.IsNullOrEmpty(gitpassword) || string.IsNullOrEmpty(gitemail))
+            {
+                StringBuilder missing = new StringBuilder();
+                if (string.IsNullOrEmpty(gitbinary))
+                    missing.AppendLine("Missing gitbinary.");
+                if (string.IsNullOrEmpty(gitserver))
+                    missing.AppendLine("Missing gitserver.");
+                if (string.IsNullOrEmpty(gitrepopath))
+                    missing.AppendLine("Missing gitrepopath.");
+                if (string.IsNullOrEmpty(gitrepofolder))
+                    missing.AppendLine("Missing gitrepofolder.");
+                if (string.IsNullOrEmpty(gitusername))
+                    missing.AppendLine("Missing gitusername.");
+                if (string.IsNullOrEmpty(gitpassword))
+                    missing.AppendLine("Missing gitpassword.");
+                if (string.IsNullOrEmpty(gitemail))
+                    missing.AppendLine("Missing gitemail.");
+
+                Log("Missing git environment variables, will not push Grafana dashboard files to Git." + Environment.NewLine + missing.ToString());
+            }
+            else
+            {
+                if (!git.Push(gitbinary, gitsourcefolder, gitserver, gitrepopath, gitrepofolder, gitusername, gitpassword, gitemail, gitsimulatepush))
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+
+        static bool ParseBooleanEnvironmentVariable(string variableName, bool defaultValue)
+        {
+            string stringValue = Environment.GetEnvironmentVariable(variableName);
+            if (stringValue == null)
+            {
+                return defaultValue;
+            }
+            else
+            {
+                bool boolValue;
+                if (!bool.TryParse(stringValue, out boolValue))
+                {
+                    return defaultValue;
+                }
+                return boolValue;
+            }
+        }
+
+        static void Log(string message)
+        {
+            BackupToGit.SecureLogger.WriteLine(message);
+        }
+    }
+}
